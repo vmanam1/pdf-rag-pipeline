db56369fe6be847877cc45b304083c7a725a3773
Vishal Manam <vmanam1@asu.edu>
2026-07-03 01:07:35 -0700

    Harden free Render deployment

--- llm-integration/llmproject/llmproject/settings.py
+++ llm-integration/llmproject/llmproject/settings.py
@@ -37,6 +37,10 @@ ALLOWED_HOSTS = [
     if host.strip()
 ]
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 # Application definition
 
